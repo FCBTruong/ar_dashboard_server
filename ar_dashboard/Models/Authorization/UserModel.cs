f946f6e892d9f9e2cab32bd5e53287cf6ee41142
Truong <55469561+FCBTruong@users.noreply.github.com>
2023-03-23 22:59:44 +0700

    update

---
+++ ar_dashboard/Models/Authorization/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ar_dashboard.Models.Authentication
+{
+    public class UserModel
+    {
+
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public int Role { get; set; }
+    }
+}
